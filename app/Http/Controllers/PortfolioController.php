a894ee73cc18cdbfbc146df76d2522a77dcfd3e3
izdlan <whaiqal7@gmail.com>
2025-12-23 17:43:36 +0800

    Initial commit: Portfolio website with Laravel

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PortfolioController extends Controller
+{
+    public function home()
+    {
+        return view('portfolio.home');
+    }
+
+    public function about()
+    {
+        return view('portfolio.about');
+    }
+
+    public function projects()
+    {
+        return view('portfolio.projects');
+    }
+
+    public function experience()
+    {
+        return view('portfolio.experience');
+    }
+
+    public function contact()
+    {
+        return view('portfolio.contact');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
+
+Route::get('/', [PortfolioController::class, 'home']);
+Route::get('/about', [PortfolioController::class, 'about']);
+Route::get('/projects', [PortfolioController::class, 'projects']);
+Route::get('/experience', [PortfolioController::class, 'experience']);
+Route::get('/contact', [PortfolioController::class, 'contact']);
